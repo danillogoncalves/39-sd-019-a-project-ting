20621c47c203d8bbb43a5bbdaaeaa1b916f472ed
danillogoncalves <danillogoncalves001@gmail.com>
2022-12-01 19:28:50 -0300

    feat: :white_check_mark: Requisito 1 feito.

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,28 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        # if not len(self._data):
+        #     return None
+        value = self._data[0]
+        del self._data[0]
+        return value
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        # if not len(self._data):
+        #     return None
+        if not 0 <= index < len(self._data):
+            raise IndexError
+        return self._data[index]
