e92c186646b9899e524f4925c90fc06281caefd8
danillogoncalves <danillogoncalves001@gmail.com>
2022-12-01 20:20:11 -0300

    feat: :white_check_mark: Requisito 2 feito.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+# https://stackoverflow.com/questions/5574702/how-do-i-print-to-stderr-in-python
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if not path_file[len(path_file) - 4:] == '.txt':
+            sys.stderr.write('Formato inválido\n')
+        else:
+            with open(path_file, encoding='utf-8', mode='r') as file:
+                return file.read().split('\n')
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
