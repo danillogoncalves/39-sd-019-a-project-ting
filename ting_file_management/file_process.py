6c6bd588b4b76f471562fec124e61201125dc682
danillogoncalves <danillogoncalves001@gmail.com>
2022-12-01 21:37:48 -0300

    feat: :white_check_mark: Requisito 3 feito.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
-def process(path_file, instance):
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
     """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return
+    list = txt_importer(path_file)
+    dist = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(list),
+        'linhas_do_arquivo': list,
+    }
+
+    instance.enqueue(dist)
+    print(dist)
 
 
 def remove(instance):
