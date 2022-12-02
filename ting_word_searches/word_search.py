a066062b307dd26d81287f9e164848d29c7406b7
danillogoncalves <danillogoncalves001@gmail.com>
2022-12-01 23:50:33 -0300

    feat: :white_check_mark: Requisito 6 feito.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
-def exists_word(word, instance):
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
     """Aqui irá sua implementação"""
+    result = []
+    for index in range(len(instance)):
+        row = instance.search(index)
+
+        list = [
+            {"linha": (position + 1)}
+            for position, line in enumerate(row['linhas_do_arquivo'])
+            if word.lower() in line.lower()
+        ]
+
+        item = {
+            "palavra": word,
+            "arquivo": row["nome_do_arquivo"],
+            "ocorrencias": list,
+        }
+
+        if list:
+            result.append(item)
+
+    return result
 
 
 def search_by_word(word, instance):
